eb1920d3924dd5a18041534a13f28ab0d1678a1d
VTariko <vv.tariko@gmail.com>
2018-03-25 18:27:31 +0300

    Исправлена ошибка в указании URL маршрута

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -38,7 +38,7 @@ namespace WebStore
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(name: "default", template: "{controller=Home/{action=Index}/{id?}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
