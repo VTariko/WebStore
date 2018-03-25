7b603ca8cae89fe96198adf06a3d222bf68c88be
VTariko <vv.tariko@gmail.com>
2018-03-26 00:04:57 +0300

    Создать контроллер, который позволяет выводить список сотрудников компании (нужно вывести ФИО и возраст) Реализовать возможность просмотра карточки выбранного сотрудника Необходимо предусмотреть возможность возврата обратно к списку

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван" ,
+                SurName = "Иванов" ,
+                Patronymic = "Иванович" ,
+                Age = 22,
+                Salary = 50000
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав" ,
+                SurName = "Петров" ,
+                Patronymic = "Иванович" ,
+                Age = 35,
+                Salary = 75000
+            }
+        };
+
         public IActionResult Index()
         {
-            return Content("Hello from controller!");
+            return View(_employees);
+        }
+
+        public IActionResult Details(int id)
+        {
+            EmployeeView employee = _employees.FirstOrDefault(e => e.Id == id);
+            return View(employee);
+            
         }
     }
 }
---
+++ WebStore/Models/EmployeeView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    /// <summary>
+    /// Класс сотрудника
+    /// </summary>
+    public class EmployeeView
+    {
+        /// <summary>
+        /// Id сотрудника
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя сотрудника
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Фамилия сотрудника
+        /// </summary>
+        public string SurName { get; set; }
+
+        /// <summary>
+        /// Отчество сотрудника
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// Возраст сотрудника
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Зарплата
+        /// </summary>
+        public int Salary { get; set; }
+    }
+}
