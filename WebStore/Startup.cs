bb823ba2c4eff456c1b0b0f530cd235be7a9a405
VTariko <vv.tariko@gmail.com>
2018-03-25 23:12:36 +0300

    Содание новго контрола - Home - и первый тест работы с ним

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.CodeAnalysis.Semantics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -37,9 +38,10 @@ namespace WebStore
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                {
+                    routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}",
+                        defaults: new {controller = "Home", action = "Index"});
+                });
         }
     }
 }
